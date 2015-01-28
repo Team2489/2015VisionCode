d97c73a7e040630ef23ee8ee6f0b017e6df0b784
Vignesh Varadarajan <vigneshv@gmail.com>
2015-01-27 21:08:39 -0800

    Filters yellow crates

--- src/vision.cpp
+++ src/vision.cpp
@@ -13,16 +13,40 @@ using namespace std;
 using namespace cv;
 using namespace cv::gpu;
 
-GpuMat yellowFilter(const GpuMat& src)
-{
-    GpuMat redOnly;
-    inRange(src, Scalar(0, 0, 0), Scalar(0, 0, 255), redOnly);
+int iLowH = 2;
+int iHighH = 157;
 
-    return redOnly;
+int iLowS = 149;
+int iHighS = 255;
+
+int iLowV = 63;
+int iHighV = 255;
+
+Mat yellowFilter(const Mat& src)
+{
+    Mat redOnly;
+    cvtColor(src, redOnly, COLOR_BGR2HSV);
+    Mat imgThresholded;
+    inRange(redOnly, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
+
+
+    return imgThresholded;
 }
 
 int main()
 {
+	namedWindow("Control", CV_WINDOW_AUTOSIZE);
+
+	cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+
+	cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+
+	cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+	cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+
+
 	int c;
 	IplImage* color_img;
     CvCapture* cv_cap = cvCaptureFromCAM(0);
@@ -34,16 +58,16 @@ int main()
 		if(color_img != 0)
 		{
 //			Mat grayscale_img;
-			GpuMat gpu_img(color_img);
+			Mat img(color_img);
 //			GpuMat gpu_grayscale_img;
 //			cvtColor(gpu_img, gpu_grayscale_img, CV_BGR2GRAY);
 //			gpu_grayscale_img.download(grayscale_img);
 
 			Mat yellow_filtered_img;
-			GpuMat filtered_img_gpu(yellowFilter(gpu_img));
-			filtered_img_gpu.download(yellow_filtered_img);
+			Mat filtered_img(yellowFilter(img));
 
-			imshow("Video", yellow_filtered_img); // show frame
+			imshow("Video", filtered_img); // show frame
+			imshow("Unprocessed", img);
 		}
 		c = cvWaitKey(10); // wait 10 ms or for key stroke
 		if(c == 27)
